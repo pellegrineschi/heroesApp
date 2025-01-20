6157abf8f3f1ba80df277784aca401e360b8e58f
pellegrineschi <pellegrineschi86@gmail.com>
2025-01-20 20:58:24 -0300

    listado de heroes

---
+++ src/app/heroes/services/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/environments';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]>{
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+}
