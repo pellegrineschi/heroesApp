cb8c692f00628fb170146bfff3e980400d62c6fb
pellegrineschi <pellegrineschi86@gmail.com>
2025-02-10 20:35:04 -0300

    auth

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { url } from 'inspector';
+import { authService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -12,6 +15,18 @@ export class LayoutPageComponent {
     {label: 'Listado', icon: 'label', url:'./list'},
     {label: 'Añadir', icon: 'add', url:'./new-hero'},
     {label: 'Buscar', icon: 'search', url:'./search'}
-  ]
+  ];
+
+  constructor(private authService: authService, private router: Router) { }
+
+  get user(): User | undefined{
+    return this.authService.currentUser
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+
+ }
 
 }
